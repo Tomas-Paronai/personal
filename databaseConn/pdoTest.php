a867f638a5bf3f54820239b29d1df00e8709143d
Tomas-Paronai <tomas.paronai@akademiasovy.sk>
2015-11-10 14:00:49 +0100

    pdo test files

---
+++ databaseConn/pdoTest.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'Database.php';
+$title = 'users';
+$name = 'Tomas';
+
+
+$handlerDB = new DBHandler();	
+
+$handlerDB->query('UPDATE users SET name=:name WHERE userid=:userid');
+$handlerDB->bind(":name","Tomas");
+$handlerDB->bind(":userid","1");
+$handlerDB->execute();
+
+$handlerDB->query('SELECT*FROM users');
+
+$users = array();
+$users = $handlerDB->resultSet();
+$count = count($users);
+
+for($i=0;$i<$count;$i++){
+	echo $users[$i]['name'],"<br/>";
+}
+
+
+?>
